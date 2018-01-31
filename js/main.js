65a66dfdf572db321cc258f7433835a10c60f9e9
TomRobertsDev <36001781+TomRobertsDev@users.noreply.github.com>
2018-01-31 15:58:50 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,25 +3,25 @@
 
 	$(function () {
 
-    /* Handle form submissions */
+	    /* Handle form submissions */
 
-    $('#contact form').submit(function(){
+	    $('#contact form').submit(function(){
 
-      /* Send form via AJAX */
+	      /* Send form via AJAX */
 
-      $.ajax({
-        url: "https://formspree.io/tomroberts.dev@gmail.com",
-        method: "POST",
-        data: {message: "hello!"},
-        dataType: "json",
-        success: function({
-          $('#contact').addClass('success');
-        });
-      });
+	      $.ajax({
+		url: "https://formspree.io/tomroberts.dev@gmail.com",
+		method: "POST",
+		data: {message: "hello!"},
+		dataType: "json",
+		success: function({
+		  $('#contact').addClass('success');
+		});
+	      });
 
-      /* Show thank you message */
+	      /* Show thank you message */
 
-    });
+	    });
 
 
 	});
